a82df17b512ebd09644fc0381808c909392e702a
Josefina Lanati <lanati.josefina@gmail.com>
2021-04-05 17:05:44 -0300

    reset css

---
+++ javascript/login.js
@@ -0,0 +1,8 @@
+var login = document.getElementsByClassName("login")
+var email = document.getElementsByClassName("email")
+var password = document.getElementsByClassName("password")
+var error = document.getElementById ("error")
+var input = document.getElementById ("password")
+var inputwo = document.getElementById("email")
+var label = document.getElementById ("password")
+var labeltwo = document.getElementById ("email")
