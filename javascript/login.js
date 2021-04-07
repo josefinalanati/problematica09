fb52cfc3ab587cecd5ff187329e7681ba42b7c16
Josefina Lanati <lanati.josefina@gmail.com>
2021-04-06 23:01:30 -0300

    change button css

--- javascript/login.js
+++ javascript/login.js
@@ -1,37 +1,11 @@
 var email = document.getElementsByClassName("email")
 var password = document.getElementsByClassName("password")
 var error = document.getElementById ("error")
-var input = document.getElementById ("password")
-var inputwo = document.getElementById("email")
-var label = document.getElementById ("password")
-var labeltwo = document.getElementById ("email")
+var labels= document.getElementsByTagName("label")
+var inputs= document.getElementsByTagName("input")
+var buttons= document.getElementsByTagName("button")
+var form = document.getElementsByClassName("containerform")
+var message = document.getElementById ("validItems")
+var title = document.getElementById("headerTitle")
+console.log(error)
 
-function validationFunction() {
-	if (email.value === '' || email.value === null) {
-		wrong.innerHTML = '<p>Please enter your email</p>';
-		wrong.style.display = 'contents';
-		wrong.style.color = '#e74c3c';
-		return;
-	}
-
-	if (
-		email.value.search(
-			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		) == -1
-	) {
-		wrong.innerHTML = '<p>The email entered is not valid</p>';
-		wrong.style.display = 'contents';
-		wrong.style.color = '#e74c3c';
-		return;
-	}
-	if (password.value === '' || password.value === null) {
-		wrong.innerHTML = '<p>Please enter a password</p>';
-		wrong.style.display = 'contents';
-		wrong.style.color = '#e74c3c';
-		return;
-	}
-	else {
-		success.innerHTML = '<p>Every validation has passed</p>';
-		success.style.display = 'contents';
-		success.style.color = '#27ae60';
-	}
